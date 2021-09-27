03fcfb7ba46add6938523b39ed38c2da7c5b90b3
Dana Sugu <dana.sugu@gmail.com>
2021-09-27 18:01:01 +0300

    update

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ draggables.forEach((draggable) => {
   draggable.addEventListener('dragstart', () => {
     draggable.classList.add('dragging');
   });
+
   draggable.addEventListener('dragend', () => {
     draggable.classList.remove('dragging');
   });
