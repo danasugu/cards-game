09930275343bd64453840fc8f81294694bc8fe31
Dana Sugu <dana.sugu@gmail.com>
2021-09-27 18:38:20 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-const draggables = document.querySelectorAll('draggable');
-const categories = document.querySelectorAll('.category');
+const draggables = document.querySelectorAll('.draggable');
+const containers = document.querySelectorAll('.container');
 
 draggables.forEach((draggable) => {
   draggable.addEventListener('dragstart', () => {
@@ -11,10 +11,34 @@ draggables.forEach((draggable) => {
   });
 });
 
-categories.forEach((category) => {
-  category.addEventListener('dragover', (e) => {
-    e.preventDefault;
-    const draggble = document.querySelector('.dragging');
-    category.appendChild(draggble);
+containers.forEach((container) => {
+  container.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    const afterElement = getDragAfterElement(container, e.clientY);
+    const draggable = document.querySelector('.dragging');
+    if (afterElement == null) {
+      container.appendChild(draggable);
+    } else {
+      container.insertBefore(draggable, afterElement);
+    }
   });
 });
+
+function getDragAfterElement(container, y) {
+  const draggableElements = [
+    ...container.querySelectorAll('.draggable:not(.dragging)'),
+  ];
+
+  return draggableElements.reduce(
+    (closest, child) => {
+      const box = child.getBoundingClientRect();
+      const offset = y - box.top - box.height / 2;
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child };
+      } else {
+        return closest;
+      }
+    },
+    { offset: Number.NEGATIVE_INFINITY }
+  ).element;
+}
