07e132628482fa03e9ee6e6e27d660ccc8e33b03
Dana Sugu <dana.sugu@gmail.com>
2021-09-27 17:21:08 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 const draggables = document.querySelectorAll('draggable');
 const abilities = document.querySelectorAll('.ability');
+
+draggables.forEach((draggable) => {
+  draggable.addEventListener('dragstart', () => {
+    draggable.classList.add('dragging');
+  });
+  draggable.addEventListener('dragend', () => {
+    draggable.classList.remove('dragging');
+  });
+});
